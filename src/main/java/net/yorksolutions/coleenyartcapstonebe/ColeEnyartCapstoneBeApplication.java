300d558728870c66737906982295b7e077b1282d
Cole <letchro@gmail.com>
2022-11-15 11:22:35 -0600

    init

---
+++ src/main/java/net/yorksolutions/coleenyartcapstonebe/ColeEnyartCapstoneBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.coleenyartcapstonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColeEnyartCapstoneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColeEnyartCapstoneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/coleenyartcapstonebe/ColeEnyartCapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.coleenyartcapstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColeEnyartCapstoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
